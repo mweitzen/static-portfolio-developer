3213b94bb0b13c6045d4834dc4cc84de5ab11b0f
Michael Weitzenhoffer <mweitzenhoffer@gmail.com>
2020-06-22 14:52:06 -0400

    styling portfolio to baseline

--- src/sections/Portfolio.js
+++ src/sections/Portfolio.js
@@ -5,12 +5,10 @@ import breakpoints from 'app/utils/breakpoints'
 import Image from '../assets/img/grayscale-bootstrap.png'
 import Section from '../components/Section'
 
-const PortfolioItemBase = styled.div`
+const PortfolioItemBase = styled.a`
   display: flex;
   height: 240px;
-  &:not(last-child) {
-    margin-bottom: 24px;
-  }
+  margin-bottom: 24px;
   &:hover {
     cursor: pointer;
     box-shadow: 0 0 6px #178578;
@@ -57,45 +55,47 @@ const PortfolioInfo = styled.div`
 `
 
 const PortfolioItem = ({ image, name, description, link }) => (
-  <a href={link} target="_blank">
-    <PortfolioItemBase>
-      <PortfolioImageContainer>
-        {/*<img alt={`picture of portfolio piece ${name}`} src={image} />*/}
-        <p>Pic</p>
-      </PortfolioImageContainer>
-      <PortfolioInfo>
-        <h3>{name}</h3>
-        <p>{description}</p>
-        <p>Tech:</p>
-        <p>Github: </p>
-        <p>Hosted URL: </p>
-      </PortfolioInfo>
-    </PortfolioItemBase>
-  </a>
+  <PortfolioItemBase href={link} target="_blank">
+    <PortfolioImageContainer>
+      {/*<img alt={`picture of portfolio piece ${name}`} src={image} />*/}
+      <p>Pic</p>
+    </PortfolioImageContainer>
+    <PortfolioInfo>
+      <h3>{name}</h3>
+      <p>{description}</p>
+      <p>Tech:</p>
+      <p>Github: </p>
+      <p>Hosted URL: </p>
+    </PortfolioInfo>
+  </PortfolioItemBase>
 )
 
 const Portfolio = () => {
   return (
     <Section name="portfolio" subheader="Some of my previous projects.">
       <PortfolioItem
+        className="portfolio-item"
         image={Image}
         name="Snake Game"
         description="Snake Game description"
         link="https://confident-perlman-3fa00d.netlify.app/"
       />
       <PortfolioItem
+        className="portfolio-item"
         image={Image}
         name="Piano Portfolio"
         description="Piano Portfolio description"
         link="https://friendly-brown-07317c.netlify.app"
       />
       <PortfolioItem
+        className="portfolio-item"
         image={Image}
         name="Video Voting Demo"
         description="Video Voting Demo description"
         link="https://gifted-kilby-f503fd.netlify.app/"
       />
       <PortfolioItem
+        className="portfolio-item"
         image={Image}
         name="Application Demo"
         description="Application Demo description"
